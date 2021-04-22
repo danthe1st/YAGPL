e680bc99bfd8bb310dd1e2e059686330e10b1b14
danthe1st <daniel@wwwmaster.at>
2021-04-22 12:14:43 +0200

    building of JARs by CI

---
+++ src/main/java/io/github/danthe1st/yagpl/ui/YAGPLLauncher.java
@@ -0,0 +1,9 @@
+package io.github.danthe1st.yagpl.ui;
+
+import javafx.application.Application;
+
+public class YAGPLLauncher {
+	public static void main(String[] args) {
+		Application.launch(YAGPL.class, args);
+	}
+}
